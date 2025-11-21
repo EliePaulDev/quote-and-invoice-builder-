1f5ccad9eda264e2ee32c843eea8c9b76cd988b9
Elie Paul <code@eliepaul.dev>
2025-11-20 20:05:11 -0500

    Added the form to quote.html, and also validation in CSS and javascript, and generate pdf document functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { jsPDF } = window.jspdf;
+
 const hamburgerMenu = document.getElementById("hamburger-menu");
 
 // let, const, var
@@ -10,4 +12,66 @@ hamburgerMenu.addEventListener('click', () => {
     const mobileMenu = document.getElementById("mobile-menu");
 
     mobileMenu.classList.toggle("hidden");
+});
+
+
+const businessEmailInput = document.getElementById("business-email");
+const clientEmailInput = document.getElementById("client-email");
+
+const businessNameInput = document.getElementById("business-name");
+const clientNameInput = document.getElementById("client-name");
+
+function valueIsEmail(value) {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(value);
+}
+
+businessEmailInput.addEventListener('blur', () => {
+    if (valueIsEmail(businessEmailInput.value)) {
+        businessEmailInput.classList.remove("invalid");
+        businessEmailInput.classList.add("valid");
+    } else {
+        businessEmailInput.classList.remove("valid");
+        businessEmailInput.classList.add("invalid");
+    }
+});
+
+businessNameInput.addEventListener('blur', () => {
+     const errorMessage = businessNameInput.previousElementSibling.querySelector(".error-message");
+    if (businessNameInput.value.trim() !== "") {
+        businessNameInput.classList.remove("invalid");
+        businessNameInput.classList.add("valid");
+        errorMessage.style.display = "none";
+    } else {
+        businessNameInput.classList.remove("valid");
+        businessNameInput.classList.add("invalid");
+        errorMessage.style.display = "inline";
+    }
+});
+
+clientNameInput.addEventListener('blur', () => {
+    if (clientNameInput.value.trim() !== "") {
+        clientNameInput.classList.remove("invalid");
+        clientNameInput.classList.add("valid");
+    } else {
+        clientNameInput.classList.remove("valid");
+        clientNameInput.classList.add("invalid");
+    }
+});
+
+const quoteForm = document.getElementById("quote-form");
+
+quoteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const quoteData = new FormData(quoteForm);
+    const quoteDateJson = Object.fromEntries(quoteData.entries());
+
+    const quoteDoc = new jsPDF();
+
+    for (let [key, value] of Object.entries(quoteDateJson)) {
+        quoteDoc.text(`${key} — ${value}`, 10, 10 + (10 * Object.keys(quoteDateJson).indexOf(key)));
+    }
+
+    quoteDoc.save("quote.pdf");
 });
